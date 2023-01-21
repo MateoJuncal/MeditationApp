03a9628c7a9dcc341ffb402f4217ee0b8f39aa5d
Mateo Juncal <mateodjm@gmail.com>
2023-01-21 05:23:01 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let minutes = 0;
+let seconds = 0;
+let started = false;
+
+const count = document.getElementById("count");
+const playButton = document.getElementById("play");
+
+const start = () => {
+  setInterval(() => {
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+    }
+
+    count.innerHTML =
+      minutes.toString().padStart(2, 0) +
+      ":" +
+      seconds.toString().padStart(2, 0);
+    seconds++;
+  }, 1000);
+  started = true;
+};
+
+startOneTime = () => {
+  if (!started) start();
+};
+
+playButton.addEventListener("click", startOneTime);
